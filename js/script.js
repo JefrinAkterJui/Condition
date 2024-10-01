c7c1084bee20d15a7169194e92872413400a4dcc
jefrinjui <jefrin.jui.dev@gmail.com>
2024-10-01 19:44:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("You are an adult.");
+// } else {
+//     console.log("You are not an adult."); // যদি শর্ত মিথ্যা হয়, এটি প্রদর্শিত হবে
+// }
